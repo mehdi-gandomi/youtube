f97cfc48b3f11dfcab65760d831aa72772050a39
mehdi <coderguy1999@gmail.com>
2018-10-28 18:48:08 +0330

    structure changed

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <main-header/>
+    <router-view/>
+    <main-footer/>
+  </div>
+</template>
+
+<script>
+import MainFooter from "@/components/partials/footer";
+import MainHeader from "@/components/partials/header";
+export default {
+  name: 'App',
+  components:{
+        MainHeader,MainFooter
+  },
+}
+</script>
+
