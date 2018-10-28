3326f6266ab176ebd6ebe86cb8cff85686869ff2
mehdi <coderguy1999@gmail.com>
2018-10-28 15:03:34 +0330

    project initialization

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <main-header/>
+    <router-view/>
+    <main-footer/>
+  </div>
+</template>
+
+<script>
+import MainFooter from "@/components/partials/footer";
+import MainHeader from "@/components/partials/header";
+export default {
+  name: 'App',
+  components:{
+        MainHeader,MainFooter
+  },
+}
+</script>
+
+<style lang="scss">
+  
+  @import "@/assets/scss/main.scss";
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,35 @@
+<template>
+            
+            <main class="wrapper">
+                <section class="video-input" >
+        <div class="description">
+                <h3>                آدرس مورد نظر خود را در کادر زیر وارد کرده و روی دکمه دانلود کلیک کنید</h3>
+
+            </div>
+            <fieldset >
+                    <label class="url-input">
+                        <input type="text" name="url" id="url" placeholder="آدرس ویدیو مورد نظر">
+                        <button  id="download-btn" ><i class="icon-download3"></i></button>
+                    </label>
+                    
+            </fieldset>
+          
+
+                    
+                </section>
+        </main>
+        
+
+</template>
+
+<script>
+
+export default {
+    
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
